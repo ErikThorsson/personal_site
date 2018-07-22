2be9ade8aa157da46520bfcbf73993770591f3c6
Erik Orndahl <eorndahl@gmail.com>
2018-07-22 18:10:12 -0400

    moved "Automate Everything" component into header; moved redundant bottom contact bar into its own component; new blog entry otw!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {BlogComponent} from "@app/blog/blog.component";
 import {PostOneComponent} from "@app/blog/post-one/post-one.component";
 import {PostTwoComponent} from "@app/blog/post-two/post-two.component";
 import {HomeComponent} from "@app/home/home.component";
+import {PostThreeComponent} from "@app/blog/post-three/post-three.component";
 
 const routes: Routes = [
     Route.withShell([
         { path: 'blog', component: BlogComponent },
         { path: 'postOne', component: PostOneComponent },
         { path: 'postTwo', component: PostTwoComponent },
+        { path: 'postThree', component: PostThreeComponent },
         { path: 'home', component: HomeComponent },
         { path: '', redirectTo: '/home', pathMatch: 'full'}
     ])
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -14,42 +14,44 @@ import { HttpCacheService } from './http/http-cache.service';
 import { ApiPrefixInterceptor } from './http/api-prefix.interceptor';
 import { ErrorHandlerInterceptor } from './http/error-handler.interceptor';
 import { CacheInterceptor } from './http/cache.interceptor';
+import {BottomBarComponent} from "@app/core/shell/bottom-bar/bottom-bar.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    HttpClientModule,
-    TranslateModule,
-    NgbModule,
-    RouterModule
-  ],
-  declarations: [
-    HeaderComponent,
-    ShellComponent
-  ],
-  providers: [
-    I18nService,
-    HttpCacheService,
-    ApiPrefixInterceptor,
-    ErrorHandlerInterceptor,
-    CacheInterceptor,
-    {
-      provide: HttpClient,
-      useClass: HttpService
-    },
-    {
-      provide: RouteReuseStrategy,
-      useClass: RouteReusableStrategy
-    }
-  ]
+    imports: [
+        CommonModule,
+        HttpClientModule,
+        TranslateModule,
+        NgbModule,
+        RouterModule
+    ],
+    declarations: [
+        HeaderComponent,
+        BottomBarComponent,
+        ShellComponent
+    ],
+    providers: [
+        I18nService,
+        HttpCacheService,
+        ApiPrefixInterceptor,
+        ErrorHandlerInterceptor,
+        CacheInterceptor,
+        {
+            provide: HttpClient,
+            useClass: HttpService
+        },
+        {
+            provide: RouteReuseStrategy,
+            useClass: RouteReusableStrategy
+        }
+    ]
 })
 export class CoreModule {
 
-  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
-    // Import guard
-    if (parentModule) {
-      throw new Error(`${parentModule} has already been loaded. Import Core module in the AppModule only.`);
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+        // Import guard
+        if (parentModule) {
+            throw new Error(`${parentModule} has already been loaded. Import Core module in the AppModule only.`);
+        }
     }
-  }
 
 }
