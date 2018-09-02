82f8012ae4b086d550f408495de991e891937799
Erik Orndahl <erik.orndahl@accesscfa.com>
2018-09-01 20:33:03 -0400

    so much styling and reactive design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, AfterViewInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit, HostListener} from '@angular/core';
 
 declare var test: any;
 
@@ -9,24 +9,71 @@ declare var test: any;
 })
 
 export class HomeComponent implements OnInit, AfterViewInit {
-    RealSense = 'RealSense <img class="icon mqtt" src="realsense.jpg" width="50">'
-    MQTT = 'MQTT <img class="icon mqtt" src="mqtt.png" width="60">'
-    Java = 'Java <i class="fab fa-java fa-3x"></i>'
-    Spring = 'Spring Boot <img class="icon" src="spring.svg" width="50">'
-    Angular = 'Angular 6 <i class="fab fa-angular fa-3x"></i>'
-    AWS = 'AWS <i class="fab fa-aws fa-3x"></i>'
-    Python = 'Python <i class="fab fa-python fa-3x"></i>'
-    HTML5 = 'HTML5 <i class="fab fa-html5 fa-3x"></i>'
-    CSS3 = 'CSS3 <i class="fab fa-css3 fa-3x"></i>'
     skillBox = new Array();
     showIt = false;
     invis: string[] = [];
-
+    RealSense = ''
+    MQTT = ''
+    Java = ''
+    Spring = ''
+    Angular = ''
+    AWS = ''
+    Python = ''
+    HTML5 = ''
+    CSS3 = ''
     constructor() {}
 
+    public innerWidth: any;
+    public innerHeight: any;
+
     ngOnInit() {
+        this.innerWidth = window.innerWidth;
+        this.reactiveDesign();
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+        this.innerWidth = window.innerWidth;
+        this.reactiveDesign();
+    }
+
+    reactiveDesign() {
+        this.skillBox = new Array();
+        if (this.innerWidth > 1000) {
+            this.skillBoxDesktop();
+        } else {
+            this.skillBoxMobile();
+            this.showIt = true;
+        }
+    }
+
+    skillBoxDesktop() {
+        this.RealSense = 'RealSense <img class="icon mqtt" src="realsense.jpg" width="50">'
+        this.MQTT = 'MQTT <img class="icon mqtt" src="mqtt.png" width="60">'
+        this.Java = 'Java <i class="fab fa-java fa-3x"></i>'
+        this.Spring = 'Spring Boot <img class="icon" src="spring.svg" width="50">'
+        this.Angular = 'Angular 6 <i class="fab fa-angular fa-3x"></i>'
+        this.AWS = 'AWS <i class="fab fa-aws fa-3x"></i>'
+        this.Python = 'Python <i class="fab fa-python fa-3x"></i>'
+        this.HTML5 = 'HTML5 <i class="fab fa-html5 fa-3x"></i>'
+        this.CSS3 = 'CSS3 <i class="fab fa-css3 fa-3x"></i>'
         this.skillBox[0] = new Array(this.Java, this.Python, this.Spring, this.AWS, this.RealSense);
         this.skillBox[1] = new Array(this.Angular, this.MQTT, this.HTML5, this.CSS3);
+    }
+
+    skillBoxMobile() {
+        this.RealSense = 'RealSense <img class="icon mqtt" src="realsense.jpg" width="180px">'
+        this.MQTT = 'MQTT <img class="icon mqtt" src="mqtt.png" width="200px">'
+        this.Spring = 'Spring Boot <img class="icon" src="spring.svg" width="180px">'
+        this.Java = 'Java <i class="fab fa-java fa-10x"></i>'
+        this.Angular = 'Angular 6 <i class="fab fa-angular fa-10x"></i>'
+        this.AWS = 'AWS <i class="fab fa-aws fa-10x"></i>'
+        this.Python = 'Python <i class="fab fa-python fa-10x"></i>'
+        this.HTML5 = 'HTML5 <i class="fab fa-html5 fa-10x"></i>'
+        this.CSS3 = 'CSS3 <i class="fab fa-css3 fa-10x"></i>'
+        this.skillBox[0] = new Array(this.Java, this.Python, this.Spring);
+        this.skillBox[1] = new Array(this.AWS, this.RealSense, this.Angular);
+        this.skillBox[2] = new Array(this.MQTT, this.HTML5, this.CSS3);
     }
 
     showWeb() {
@@ -73,11 +120,18 @@ export class HomeComponent implements OnInit, AfterViewInit {
         return style;
     }
 
+    getResumeScreenStyle() {
+        this.innerWidth = window.innerWidth;
+        let style: Object = null;
+        if (this.innerWidth < 1000) {
+            style = {'font-size': '200%'};
+        }
+        return style;
+    }
 
     ngAfterViewInit() {
          // new test();
     }
-
 
     showResume() {
         if (!this.showIt) {
