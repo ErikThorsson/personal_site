1571c56ebd089358dd5cbc354a5a90dd00239662
Erik Orndahl <erik.orndahl@accesscfa.com>
2019-05-08 22:39:59 -0400

    touch up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,12 @@ import {BlogComponent} from '@app/blog/blog.component';
 import {PostOneComponent} from '@app/blog/post-one/post-one.component';
 import {PostTwoComponent} from '@app/blog/post-two/post-two.component';
 import {HomeComponent} from '@app/home/home.component';
-import {PostThreeComponent} from '@app/blog/post-three/post-three.component';
-import {PostFourComponent} from '@app/blog/post-four/post-four.component';
-import {TradesComponent} from '@app/blog/post-four/trades.component';
 
 const routes: Routes = [
     Route.withShell([
         { path: 'blog', component: BlogComponent },
         { path: 'postOne', component: PostOneComponent },
         { path: 'postTwo', component: PostTwoComponent },
-        { path: 'postThree', component: PostThreeComponent },
-        { path: 'postFour', component: PostFourComponent },
-        { path: 'trades', component: TradesComponent },
         { path: 'home', component: HomeComponent },
         { path: '', redirectTo: '/home', pathMatch: 'full'}
     ])
